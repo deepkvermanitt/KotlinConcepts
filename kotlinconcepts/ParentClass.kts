7338fa247d0e3a3ebe8b451e36f54805a3f2ae5f
TFDeep <deep.nitt@gmail.com>
2025-04-01 09:38:25 +0530

    Add main method and add comment for better explaining

--- kotlinconcepts/ParentClass.kts
+++ kotlinconcepts/ParentClass.kts
@@ -1,23 +1,28 @@
-open class ParentClass{
-  open  var name:String = "Parent"
-    constructor(name:String){
-        this.name = name
-    }
-  open  fun printName(){
-        println(name)
+/**
+ * Classes → open ka matlab hai inheritance allow karna. By default, Kotlin classes final hoti hain, to subclass nahi bana sakte bina open lagaye.
+ *
+ * Functions → open ka matlab hai function ko override kar sakna subclass me. Agar open nahi hoga, to override nahi kar sakte.
+ *
+ * Variables (Properties) → open lagane se subclass us property ko override kar sakta hai.
+ */
+open class Parent {
+    open var name: String = "Parent"
+
+    open fun greet() {
+        println("Hello from Parent")
     }
 }
 
-class ChildClass : ParentClass{
+class Child : Parent() {
+    override var name: String = "Child"
 
-
-    constructor(name:String):super(name){
-
+    override fun greet() {
+        println("Hello from Child")
     }
+}
 
-    override var name:String = "Child"
-
-    override fun printName(){
-
-    }
+fun main(args: Array<String>) {
+    val child = Child()
+    println(child.name)
+    child.greet()
 }
